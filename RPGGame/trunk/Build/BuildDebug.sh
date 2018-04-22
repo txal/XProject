ccb6a067c5754b5c1c4e50ef6fad229e86b779dc
root <root@localhost.localdomain>
2018-04-22 22:03:46 +0800

    AAA

--- RPGGame/trunk/Build/BuildDebug.sh
+++ RPGGame/trunk/Build/BuildDebug.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
-make clean
+#make clean
 cmake -DCMAKE_BUILD_TYPE=Debug ../
 make
+
+echo ****** copy 'LogicServer' to _WorldServer/ ******
+cp ../Bin/_LocalServer/LogicServer ../Bin/_WorldServer/
