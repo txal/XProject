98ed06f0176e23285996fdf01019d9cff6b956c8
root <root@localhost.localdomain>
2018-08-16 16:02:44 +0800

    AAA

--- RPGGame/trunk/Build/BuildDebug.sh
+++ RPGGame/trunk/Build/BuildDebug.sh
@@ -2,6 +2,3 @@
 make clean
 cmake -DCMAKE_BUILD_TYPE=Debug ../
 make
-
-cp ../Bin/_LocalServer/LogicServer ../Bin/_WorldServer/
-cp ../Bin/_WorldServer/WGlobalServer ../Bin/_WorldServer/WGlobalServer2
--- RPGGame/trunk/Build/BuildRelease.sh
+++ RPGGame/trunk/Build/BuildRelease.sh
@@ -2,6 +2,3 @@
 make clean
 cmake -DCMAKE_BUILD_TYPE=Release ../
 make
-
-cp ../Bin/_LocalServer/LogicServer ../Bin/_WorldServer/
-cp ../Bin/_WorldServer/WGlobalServer ../Bin/_WorldServer/WGlobalServer2
